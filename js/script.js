0744086549a303feb76f34c7b4ed37a787274354
Vitaliy <vitaliy.sh.82@gmail.com>
2019-11-12 22:16:24 +0200

    Send form data in JSON

--- js/script.js
+++ js/script.js
@@ -118,6 +118,53 @@ window.addEventListener('DOMContentLoaded', function(){
     const item = new Options(300, 350, "red", 14, "center");
     item.createDiv();
 
+    //____Form______
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Дякуємо, найближчим часом ми вам зателефонуємо',
+        failure: 'Err...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        
+        statusMessage.classList.add('status');
+    
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        let formData = new FormData(form);
+        
+        let obj = {};
+            formData.forEach(function(value, key){
+            obj[key] = value;
+            });
+        
+        let json = JSON.stringify(obj);
+            request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            }else{
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        
+        for (let i=0; i<input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
 });
 
  
